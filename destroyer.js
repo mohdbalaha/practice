b7c7d88d53d645801abf741078bf1029ae482739
Mohamad Balaha <mohdbalaha@yahoo.co.jp>
2018-04-16 00:18:22 +0900

    destroyer

---
+++ destroyer.js
@@ -0,0 +1,20 @@
+
+function destroyer(arr) {
+  // Remove all the values
+  let args = [];
+  for (let i = 1; i < arguments.length; i++) {
+    args.push(arguments[i]);
+  }
+
+  return arr.filter(function (item) {
+    for (let x = 0; x < args.length; x++) {
+      return args.indexOf(item) === -1;
+    }
+  });
+
+}
+
+console.log(destroyer([1, 2, 3, 1, 2, 3], 2, 3));
+console.log(destroyer([3, 5, 1, 2, 2], 2, 3, 5));// should return [1].
+console.log(destroyer([2, 3, 2, 3], 2, 3));// should return [].
+console.log(destroyer(["tree", "hamburger", 53], "tree", 53));// should return ["hamburger"].
